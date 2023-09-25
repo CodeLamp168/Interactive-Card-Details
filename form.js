5a9da2f53f8f65ab714c80aff93854979a9a65b6
Caleb D. Sim <dylansim168@gmail.com>
2023-09-25 19:38:45 +0800

    revised form function (wip)

--- form.js
+++ form.js
@@ -1,149 +1,94 @@
+const form = document.getElementById("cardForm");
+const cardHolderDisplay = document.getElementById("credit-card-holder");
+const cardNumberDisplay = document.getElementById("credit-card-number");
+const cardMonthDisplay = document.getElementById("exp-m");
+const cardYearDisplay = document.getElementById("exp-y");
+const cardCvcDisplay = document.getElementById("credit-card-cvc");
 
-const cardHolderNameInput = document.getElementById('cardholderName');
-const cardNumberInput = document.getElementById('cardNumber');
-const expMonthInput = document.getElementById('expMonth');
-const expYearInput = document.getElementById('expYear');
-const cvcInput = document.getElementById('cvc');
+const usernameElement = document.getElementById("cardholderName");
+const cardNumberElement = document.getElementById("cardNumber");
+const expMonthElement = document.getElementById("expMonth");
+const expYearElement = document.getElementById("expYear");
+const cvcElement = document.getElementById("cvc");
 
-const cardDisplayName = document.getElementById('credit-card-holder');
-const cardDisplayNumber = document.getElementById('credit-card-number');
-const cardDisplayMonth = document.getElementById('exp-m');
-const cardDisplayYear = document.getElementById('exp-y');
-const cardDisplayCVC = document.getElementById('credit-card-cvc');
+//validation
+const userNameValidation = /^[A-Za-z ]+$/;
+const cardNumberValidation = /^\d{16}$/;
+const monthValidation = /^(0[1-9]|1[0-2])$/;
+const yearValidation = /^\d{2}$/;
+const cvcValidation = /^\d{3}$/;
 
-const cardHolderValidation = /^[A-Za-z\s]+$/
-const cardNumberValidation = /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/
-const cardMonthValidation = /^(0[1-9]|1[0-2])$/
-const cardYearValidation = /^\d{2}$/
-const cardCVCValidation = /^\d{3}$/
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-const form = document.getElementById('cardForm');
-const completeContent = document.getElementById('thank-you-display')
-const submitButton = document.getElementById('submitButton');
-const continueButton = document.getElementById('continue-btn')
+  const username = form.cardholderName.value;
+  const cardnumber = form.cardNumber.value;
+  const monthnumber = form.expMonth.value;
+  const yearnumber = form.expYear.value;
+  const cvcnumber = form.cvc.value;
 
-const errorDiv = document.createElement('div')
-errorDiv.className = 'error-notification'
+  //TODO
+  if (userNameValidation.test(username)) {
+    console.log(`${username} is valid`);
+  } else {
+    console.log(`${username} is not valid`);
+  }
 
+  if (cardNumberValidation.test(cardnumber)) {
+    console.log(`${cardnumber} is valid`);
+  } else {
+    console.log(`${cardnumber} is not valid`);
+  }
 
+  if (monthValidation.test(monthnumber)) {
+    console.log(`${monthnumber} is valid`);
+  } else {
+    console.log(`${monthnumber} is not valid`);
+  }
 
+  if (yearValidation.test(yearnumber)) {
+    console.log(`${yearnumber} is valid`);
+  } else {
+    console.log(`${yearnumber} is not valid`);
+  }
 
+  if (cvcValidation.test(cvcnumber)) {
+    console.log(`${cvcnumber} is valid`);
+  } else {
+    console.log(`${cvcnumber} is not valid`);
+  }
+});
 
+const handleInput = (validationRegex, inputDisplay) => (e) => {
+    const inputValue = e.target.value;
+  
+    if(e.target === usernameElement){
+        if (inputValue.length > 28) {
+            inputDisplay.innerText = inputValue.slice(0, 28) + "...";
+          } else {
+            inputDisplay.innerText = inputValue;
+        }
+    }
 
-function validation(inputElement, cardDisplay, regexPattern, errorText) {
-    const value = inputElement.value.trim();
-    const cardDisplayOriginalHTML = cardDisplay.innerHTML;
-    inputElement.parentElement.appendChild(errorDiv);
+    if(e.target === cardNumberElement){
+        inputDisplay.innerText = inputValue.replace(/(\d{4})(?=\d)/g, '$1 ');
+    } 
 
+    inputDisplay.innerText = inputValue
     
-
-    if (value === '') {
-        inputElement.classList.remove('valid', 'invalid');
-        errorDiv.classList.add('notification-hide')
-        inputElement.classList.add('empty');
-        cardDisplay.textContent = cardDisplayOriginalHTML;
-
-    }
-    else if (regexPattern.test(value)) {
-        inputElement.classList.remove('invalid');
-        inputElement.classList.add('valid');
-        errorDiv.classList.remove('invalid-notification')
-        errorDiv.classList.add('notification-hide')
-        cardDisplay.textContent = value;
-
+        
+    if (validationRegex.test(inputValue)) {
+      e.target.classList.add('valid-input');
+      e.target.classList.remove('invalid-input');
     } else {
-        inputElement.classList.remove('valid');
-        inputElement.classList.add('invalid');
-        errorDiv.classList.remove('notification-hide')
-        errorDiv.classList.add('invalid-notification')
-        cardDisplay.textContent = cardDisplayOriginalHTML;
-        errorDiv.textContent = errorText
+      e.target.classList.add('invalid-input');
+      e.target.classList.remove('valid-input');
     }
-
-    inputElement.setAttribute('data-true-value', value);
-
-}
-
-function formatCardNumber(inputElement) {
-    const value = inputElement.value.replace(/\s/g, '');
-    const formattedValue = [];
-
-    for (let i = 0; i < value.length; i++) {
-        if (i > 0 && i % 4 === 0) {
-            formattedValue.push(' '); 
-        }
-        formattedValue.push(value[i]);
-    }
-
-    inputElement.value = formattedValue.join('');
-
-    inputElement.setAttribute('data-true-value', value);
-}
-
-
-
-cardHolderNameInput.addEventListener('input', () => {
-    validation(cardHolderNameInput, cardDisplayName, cardHolderValidation, 'Please enter alphabetic characters only');
-});
-
-cardNumberInput.addEventListener('input', () => {
-    formatCardNumber(cardNumberInput); 
-    validation(cardNumberInput, cardDisplayNumber, cardNumberValidation, 'Please enter 16 numeric digits only'); 
-    resetFunction(cardNumberInput)
-    const trueValue = cardNumberInput.getAttribute('data-true-value');
-});
-
-expMonthInput.addEventListener('input', () => {
-    validation(expMonthInput, cardDisplayMonth, cardMonthValidation, 'Must be filled');
-    resetFunction(cardNumberInput)
-});
-
-expYearInput.addEventListener('input', () => {
-    validation(expYearInput, cardDisplayYear, cardYearValidation, 'Must be filled');
-});
-
-cvcInput.addEventListener('input', () => {
-    validation(cvcInput, cardDisplayCVC, cardCVCValidation, 'Must be filled');
-});
-
-
-
-function areAllInputsValid() {
-    const inputs = form.querySelectorAll('input:not([type="submit"])');
-    for (const input of inputs) {
-        if (!input.classList.contains('valid')) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
-form.addEventListener('input', () => {
-    if (areAllInputsValid()) {
-        submitButton.removeAttribute('disabled');
-    } else {
-        submitButton.setAttribute('disabled', 'true');
-    }
-});
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    form.style.display = 'none'
-    completeContent.style.display = 'flex'
-    console.log('Form submitted successfully!');
-});
-
-continueButton.addEventListener('click', () => {
-    location.reload();
-    console.log('button pressed');
-});
-
-window.addEventListener('load', function () {
-    cardHolderNameInput.value = '';
-    cardNumberInput.value = '';
-    expMonthInput.value = '';
-    expYearInput.value = '';
-    cvcInput.value = '';
-  });
+  };
+  
+  
+  usernameElement.addEventListener("keyup", handleInput(userNameValidation, cardHolderDisplay));
+  cardNumberElement.addEventListener("keyup", handleInput(cardNumberValidation, cardNumberDisplay));
+  expMonthElement.addEventListener("keyup", handleInput(monthValidation, cardMonthDisplay));
+  expYearElement.addEventListener("keyup", handleInput(yearValidation, cardYearDisplay));
+  cvcElement.addEventListener("keyup", handleInput(cvcValidation, cardCvcDisplay));
